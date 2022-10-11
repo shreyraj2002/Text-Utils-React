f6f27a0a3c88295f2a0c25c715e5caa1e1c57b4e
Shrey Raj <shreyraj2002@gmail.com>
2022-10-11 17:51:06 +0530

    Added new functions

--- src/Component/Textform.js
+++ src/Component/Textform.js
@@ -8,20 +8,39 @@ export default function Textform(props) {
         setText(newText);
     }
 
+    const handleDownClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event) => {
         // console.log("On Change");
         setText(event.target.value);
     }
 
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
 
     return (
+        <>
         <div>
             <h1>{props.headings}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="15"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to Lowercase</button>
         </div>
+
+        <div className="container my-3">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words, {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes to read.</p>
+
+            <h2>Preview</h2>
+            <p>{text}</p>
+
+        </div>
+
+        </>
     )
 }
